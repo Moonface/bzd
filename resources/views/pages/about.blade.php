cb884d0a85ad99380443d9fd7eeedcc8dea25148
Moonface <kar.romanowski@gmail.com>
2017-09-06 16:51:45 +0200

    Initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,24 @@
+@extends('master')
+@section('content')
+
+    <div class="page-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-lg-6"><h3>Przykładowy tytuł strony</h3>
+
+
+                                To jest strona O nas.
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@stop
